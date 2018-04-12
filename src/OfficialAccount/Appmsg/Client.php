7bd49c4d595f3eb98c3e5a36e5ffaa3b1329d90a
aoc <aoc@aoc.com>
2018-04-12 11:27:44 +0800

    修改图文素材请求参数方式

--- src/OfficialAccount/Appmsg/Client.php
+++ src/OfficialAccount/Appmsg/Client.php
@@ -16,16 +16,9 @@ use JinWeChat\Kernel\BaseClient;
 class Client extends BaseClient
 {
     /**
-<<<<<<< HEAD
      * 图文素材列表
      * @param $begin
      * @param $count
-=======
-     * 图文素材列表.
-     *
-     * @param $query
-     *
->>>>>>> 89a68d3d1fb6e06e14839c3e85267700a0aa95bd
      * @return \Psr\Http\Message\ResponseInterface
      *
      * @throws \JinWeChat\Kernel\Exceptions\InvalidConfigException
